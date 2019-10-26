5942afcfa3e8cafd8b2e629ea0f4227ef7385301
combatd <combatdstar@gmail.com>
2019-10-26 13:01:55 -0700

    add router sdfsdfdsfsfsd'

--- controllers/users.js
+++ controllers/users.js
@@ -3,26 +3,30 @@ const router = express.Router();
 const User = require('../models/users');
 const bcrypt = require('bcryptjs');
 
+
 router.post('/register', async (req, res) => {
 
     const password = req.body.password;
     // hash a password in a generated salt
-
+    const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
     const userDbEntry = {};
     // information will come from the form that follows the 
     // userSchema from models/users.js
-
-
+    userDbEntry.username = req.body.username;
+    userDbEntry.password = req.body.password;
+    userDbEntry.email = req.body.email;
 
     // we can actually add the user to the db here
     // set the username taken from the session to the new user's
     // username and then log the session as online
-
+    const createdUser = await User.create(userDbEntry);
+    console.log(createdUser);
+    req.session.username;
+    req.session.logged = true;
 
     
-
-    // res.redirect('/posts');
+    res.redirect('/posts');
 });
 
 router.post('/login', async (req, res) => {
