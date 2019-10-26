c5b08d41ee1925162f6ad0a9849805a8c2e8a1e8
combatd <combatdstar@gmail.com>
2019-10-26 16:14:24 -0700

    require modules and schemas at top of controllers/posts.js

---
+++ controllers/posts.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/posts');
+const Comment = require('../models/comments');
+
+
+
+
+
+
+
+
+module.exports = router;
