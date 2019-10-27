a24723f317462f5c12c12fa519c8f6a917b0625b
combatd <combatdstar@gmail.com>
2019-10-26 17:29:46 -0700

    create Posts - Index route, needs view template

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,7 +3,19 @@ const router = express.Router();
 const Post = require('../models/posts');
 const Comment = require('../models/comments');
 
-
+router.get('/', (req, res) => {
+    console.log(req.session);
+    Post.find({}, (err, foundPosts) => {
+        if(err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            res.render('posts/index.ejs', {
+                posts: foundPosts
+            });
+        }
+    });
+});
 
 
 
