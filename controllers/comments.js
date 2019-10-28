843cec9cf65b55c272990a948ab8253c9d56e6b9
combatd <combatdstar@gmail.com>
2019-10-28 14:37:01 -0700

    make logic for Comment - Create (POST)

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,3 +3,74 @@ const router = express.Router();
 const User = require('../models/users');
 const Post = require('../models/posts');
 
+// Comment - New (GET)
+router.get('posts/:id/newcomment', (req, res) => {
+    Post.findById(req.params.id, (err, foundPost) => {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            res.render('comments/new.ejs', {
+                post: foundPost
+            });
+        }
+    });
+});
+
+router.post('/posts/:id', (req, res) => {
+    Comment.create(req.body, (err, createdComment) => {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            console.log(req.body)
+
+            // find the user by its username, then push the post
+            // into the posts array
+            Post.findById(req.params.id, (err, foundPost) => {
+                
+                const foundUser = User.findOne({'posts': req.params.id});
+
+                if (err) {
+                    console.log(err);
+                    res.send(err);
+                } else {
+
+                    foundPost.comments.push(createdComment);
+                    // if we mutate a document we have to save it
+
+                    foundPost.save((err, savedPost) => {
+                        console.log(savedPost)
+                        // update the user as well
+                        foundUser.save((err, savedUser) => {
+                            console.log(savedUser);
+                        });
+
+                        res.redirect('/posts/' + req.params.id);
+                    });
+
+                    
+                    // when we are dealin
+                }
+            });
+        }
+    });
+});
+
+// // Comment - Put Route
+// router.put('/:id', (req, res) => {
+//     Post.findByIdAndUpdate(req.params.id, req.body, (err, response) => {
+//         if (err) {
+//             console.log(err);
+//             res.send(err);
+//         } else {
+//             res.redirect('/posts/' + req.params.id);
+//         }
+
+//     });
+// });
+
+
+
+
+module.exports = router;
